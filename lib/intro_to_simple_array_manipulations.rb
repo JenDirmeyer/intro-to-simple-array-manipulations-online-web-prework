ca33970663f5c99b41c3ffc759b75687c85ed2ad
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-30 12:53:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,3 +10,15 @@ def using_pop(array)
   array.pop 
 end  
   
+def pop_with_args (array)
+  array.pop(2)
+end
+
+def using_shift(array, string)
+  array.shift(string)
+end
+
+def shift_with_args(array, string)
+  array.shift(2)
+end 
+
